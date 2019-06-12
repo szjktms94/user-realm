d97b6393aa45a0469fb3b0dafdfa02a35f25f3a2
Tomi <szjktms94@gmail.com>
2019-06-12 22:38:05 +0100

    TASK-01: fix missing class

---
+++ src/main/java/com/brighttalk/userrealm/model/RealmResponseInterface.java
@@ -0,0 +1,5 @@
+package com.brighttalk.userrealm.model;
+
+//Interface for UserRealm and Error class because both of them can be return value for the same controller method
+public interface RealmResponseInterface {
+}
